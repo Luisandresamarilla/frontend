708c3dc3d4171afdfd19f601f4608dc5416dc72d
Luisandresamarilla <73044516+Luisandresamarilla@users.noreply.github.com>
2020-10-19 04:02:04 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function cartel() {alert("Potrero Digital")}
+
+function cambiarfondo1() { document.body.style.backgroundColor = "red";}
+
+function cambiarfondo2() {
+  document.body.style.backgroundColor = "blue";
+}
+alert("Hola Potrero")
+
+function roberto() {
+  document.getElementById("luis").innerHTML = "Roberto Andres";
+}
+function AmpliarImg(x) {
+  x.style.height = "20%";
+  x.style.width = "20%";
+}
+
+function NormalImg(x) {
+  x.style.height = "10%";
+  x.style.width = "10%";
+}
